d4d51623cdd8fa75e2582b5aeaba092a21224edd
kriskirla <kris.lai@mail.utoronto.ca>
2017-06-23 11:28:36 -0400

    First commit

---
+++ ChakBot/ChakBot/Form1.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Text.RegularExpressions;
+
+namespace ChakBot
+{
+    public partial class Form1 : Form
+    {
+        List<string> temptText = new List<string>() {};
+        int historyIndex = 0;
+        int prevIndex = 0;
+        Random rnd = new Random();
+        string PATH = System.IO.Directory.GetCurrentDirectory() + "/brain.txt";
+
+        // Variables to store responses
+        Dictionary<string, List<string>> categories = new Dictionary<string, List<string>>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.AcceptButton = Enter;
+            this.ActiveControl = InputChat;
+            this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+
+            // Read Text file to get responses
+            // If brain.txt DNE, then create a blank new one
+            if (!System.IO.File.Exists(PATH))
+            {
+                using (System.IO.File.Create(PATH)) { }
+            }
+
+            System.IO.StreamReader file = new System.IO.StreamReader(PATH);
+
+            // Read line and put into list
+            string line;
+            int counter = 0;
+            string categ;
+            List<string> content;
+
+            while ((line = file.ReadLine())!= null)
+            {
+                categ = line.Substring(0, line.IndexOf("="));
+                content = line.Substring(line.IndexOf("=") + 1, line.Length - line.IndexOf("=") - 1).Split(';').ToList();
+                categories[categ] = content;
+                //categories.Add(categ, content);
+                counter++;
+            }
+
+            file.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void OutputChat_TextChanged(object sender, EventArgs e)
+        {
+            OutputChat.SelectionStart = OutputChat.Text.Length;
+            OutputChat.ScrollToCaret();
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            System.IO.StreamWriter file = new System.IO.StreamWriter(System.IO.Directory.GetCurrentDirectory() +
+                "/ChakBot Conversaion History.txt");
+            file.WriteLine(OutputChat.Text.Replace(System.Environment.NewLine, "\\r\\n"));
+            file.Close();
+            MessageBox.Show("File has been saved to current directory.");
+        }
+
+        /// <summary>
+        /// When Enter is clicked, many operations happen.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Enter_Click(object sender, EventArgs e)
+        {
+            // Output user message
+            OutputChat.AppendText("User:\r\n >>> " + InputChat.Text + "\r\n");
+
+            // Add convo to history
+            temptText.Add(InputChat.Text);
+            historyIndex++;
+
+            // Refresh Chatbox
+            InputChat.Text = "";
+            prevIndex = historyIndex;
+            OutputChat.Refresh();
+            InputChat.Refresh();
+
+            // Call Chakbot to respond
+            ChakBot_Convo();
+        }
+
+        /// <summary>
+        /// Robot Response
+        /// </summary>
+        private async void ChakBot_Convo()
+        {
+            for (int i = 0; i < rnd.Next(1, 3); i++)
+            {
+                TypingDisplay.Text = "ChakBot is typing.";
+                await Task.Delay(500);
+                TypingDisplay.Text = "ChakBot is typing..";
+                await Task.Delay(500);
+                TypingDisplay.Text = "ChakBot is typing...";
+                await Task.Delay(500);
+            }
+
+            if (temptText[historyIndex - 1].Substring(0, temptText[historyIndex - 1].IndexOf('=') + 1).ToLower() == "teach=")
+            {
+                teach_chakbot(temptText[historyIndex - 1].Substring(6,
+                    temptText[historyIndex - 1].Length - 6));
+            }
+            else
+            {
+                OutputChat.AppendText("ChakBot:\r\n >>> " +
+                    getMessage(temptText[historyIndex - 1]) + "\r\n");
+                TypingDisplay.Text = "";
+            }
+        }
+
+        /// <summary>
+        /// Key detection
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                if (prevIndex > 0)
+                {
+                    InputChat.Text = temptText[prevIndex - 1];
+                    if (prevIndex != 0) { prevIndex--; }
+                }
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                if (prevIndex < historyIndex - 1)
+                {
+                    InputChat.Text = temptText[prevIndex + 1];
+                    if (prevIndex != historyIndex - 1) { prevIndex++; }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Brain of Robot
+        /// </summary>
+        /// <param name="feed"></param>
+        /// <returns></returns>
+        private string getMessage(string feed)
+        {
+            // Analyze by word
+            Regex rgx = new Regex("[^a-zA-Z0-9 -]");
+            string sentence = rgx.Replace(feed, "").ToLower();
+            string root;
+            string cor;
+      
+            // === Response Decider ===
+            // First see if sentence exist, if not then decide by word
+            foreach (string categ in categories.Keys)
+            {
+                // Find the corresponding answer (only applies to requests)
+                if (categ.Contains(';'))
+                {
+                    root = categ.Substring(0, categ.IndexOf(';'));
+                    cor = categ.Substring(categ.IndexOf(';') + 1, categ.Length - categ.IndexOf(';') - 1);
+
+                    // If the root and cor is the same, then remain same
+                    if (root == cor)
+                    {
+                        cor = categ;
+                    }
+
+                    if (categories[categ].Contains(sentence))
+                    {
+                        return categories[cor][rnd.Next(0, categories[cor].Count())];
+                    }
+                }
+            }
+
+            return "Sorry, I did not learn how to answer that =[. Please teach me by putting: teach=categ;corres>question>Rcateg>reponse";
+        }
+
+        /// <summary>
+        /// Adding info to ckabot's brain
+        /// </summary>
+        private void teach_chakbot(string learn)
+        {
+            // Format of teaching
+            // categ>question>response
+            List<string> disect = learn.Split('>').ToList();
+
+            // Check format
+            if (disect[0] == "" || disect[1] == "" || disect[2] == "" || disect[3] == "")
+            {
+                OutputChat.AppendText("ChakBot:\r\n >>> " +
+                    "Please enter the correct format: teach=categ;corres>question>Rcateg>reponse" + "\r\n");
+            }
+            else
+            {
+                // If category exist
+                if (categories.Keys.Contains(disect[0] + ";" + disect[2]))
+                {
+                    var text = new StringBuilder();
+
+                    foreach (string s in System.IO.File.ReadAllLines(PATH))
+                    {
+                        if (s.Substring(0, s.IndexOf("=")) == disect[0] + ";" + disect[2])
+                        {
+                            if (disect[0] == disect[2])
+                            {
+                                text.AppendLine(s.Replace(s, s + ";" + disect[1] + ";" + disect[3]));
+                            }
+                            else
+                            {
+                                text.AppendLine(s.Replace(s, s + ";" + disect[1]));
+                            }
+                        }
+                        else
+                        {
+                            text.AppendLine(s);
+                        }
+                    }
+
+                    using (var file = new System.IO.StreamWriter(PATH))
+                    {
+                        file.Write(text.ToString());
+                        file.Close();
+                    }
+                }
+                // Otherwise
+                else
+                {
+                    System.IO.File.AppendAllText(PATH,
+                            disect[0] + ";" + disect[2] + "=" + disect[1] + Environment.NewLine);
+                }
+
+                if (disect[0] != disect[2])
+                {
+                    // If category exist
+                    if (categories.Keys.Contains(disect[2]))
+                    {
+                        var text = new StringBuilder();
+
+                        foreach (string s in System.IO.File.ReadAllLines(PATH))
+                        {
+                            if (s.Substring(0, s.IndexOf("=")) == disect[2])
+                            {
+                                text.AppendLine(s.Replace(s, s + ";" + disect[3]));
+                            }
+                            else
+                            {
+                                text.AppendLine(s);
+                            }
+                        }
+
+                        using (var file = new System.IO.StreamWriter(PATH))
+                        {
+                            file.Write(text.ToString());
+                            file.Close();
+                        }
+                    }
+                    // Otherwise
+                    else
+                    {
+                        System.IO.File.AppendAllText(PATH,
+                                disect[2] + "=" + disect[3] + Environment.NewLine);
+                    }
+                }
+
+                OutputChat.AppendText("ChakBot:\r\n >>> " +
+                        "Thanks! I learned it! =D" + "\r\n");
+                TypingDisplay.Text = "";
+            }
+
+            // Reread content
+            categories.Clear();
+
+            System.IO.StreamReader read = new System.IO.StreamReader(PATH);
+
+            // Read line and put into list
+            string line;
+            int counter = 0;
+            string categ;
+            List<string> content;
+
+            while ((line = read.ReadLine()) != null)
+            {
+                categ = line.Substring(0, line.IndexOf("="));
+                content = line.Substring(line.IndexOf("=") + 1, line.Length - line.IndexOf("=") - 1).Split(';').ToList();
+                categories[categ] = content;
+                //categories.Add(categ, content);
+                counter++;
+            }
+
+            read.Close();
+        }
+    }
+}
---
+++ ConversionTool/ConversionTool/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+
+using System.Diagnostics;
+
+namespace ConversionTool
+{
+    public partial class ConversionTool : Form
+    {
+        public ConversionTool()
+        {
+            InitializeComponent();
+        }
+
+        private void ConversionTool_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        /// <summary>
+        /// TAB1: This is solely for convert lengths
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void LengthConvert_Click(object sender, EventArgs e)
+        {
+            // When clicked, use the selected item as default ratio
+            StaticVariables metric = new StaticVariables();
+            double input1 = (double)LengthInputBox1.Value;
+            double input2 = (double)LengthInputBox2.Value;
+
+            LengthOutput1.Text = metric.getValue(input1, input2,
+                LengthSelect1.Text, LengthSelect2.Text, LengthBox1.Text);
+
+            LengthOutput2.Text = metric.getValue(input1, input2,
+                LengthSelect1.Text, LengthSelect2.Text, LengthBox2.Text);
+
+            LengthOutput3.Text = metric.getValue(input1, input2,
+                LengthSelect1.Text, LengthSelect2.Text, LengthBox3.Text);
+
+            LengthOutput4.Text = metric.getValue(input1, input2,
+                LengthSelect1.Text, LengthSelect2.Text, LengthBox4.Text);
+
+            LengthOutput5.Text = metric.getValue(input1, input2,
+                LengthSelect1.Text, LengthSelect2.Text, LengthBox5.Text);
+
+            LengthOutput6.Text = metric.getValue(input1, input2,
+                LengthSelect1.Text, LengthSelect2.Text, LengthBox6.Text);
+            
+        }
+
+        private void LengthSelect1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (LengthSelect1.Text != "" || LengthSelect2.Text != "")
+            {
+                LengthConvert.Enabled = true;
+            }
+            else
+            {
+                LengthConvert.Enabled = false;
+            }
+        }
+
+        private void LengthSelect2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (LengthSelect2.Text != "" || LengthSelect1.Text != "")
+            {
+                LengthConvert.Enabled = true;
+            }
+            else
+            {
+                LengthConvert.Enabled = false;
+            }
+        }
+
+        private void LengthCopyButton_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Clipboard.SetText(readHelper());
+            MessageBox.Show("Successfully copied to clipboard.");
+        }
+
+        private void FileSaveButton_Click(object sender, EventArgs e)
+        {
+            System.IO.StreamWriter file = new System.IO.StreamWriter(System.IO.Directory.GetCurrentDirectory() + 
+                "/Length Metric Conversion.txt");
+            file.WriteLine(readHelper());
+            file.Close();
+            MessageBox.Show("File has been saved to current directory.");
+        }
+
+        private string readHelper()
+        {
+            string result = "";
+
+            for (int i = 1; i < 3; i++)
+            {
+                string unit = ((ComboBox)this.GetType().GetField("LengthSelect" + i,
+                BindingFlags.NonPublic | BindingFlags.Instance).GetValue(this)).Text;
+                string input = ((NumericUpDown)this.GetType().GetField("LengthInputBox" + i,
+                BindingFlags.NonPublic | BindingFlags.Instance).GetValue(this)).Text;
+
+                if (unit != "")
+                {
+                    result += input + " " + unit + "\r\n";
+                }
+            }
+
+            result += "=\r\n";
+
+            for (int i = 1; i < 7; i++)
+            {
+                string unit = ((ComboBox)this.GetType().GetField("LengthBox" + i,
+                BindingFlags.NonPublic | BindingFlags.Instance).GetValue(this)).Text;
+                string output = ((TextBox)this.GetType().GetField("LengthOutput" + i,
+                BindingFlags.NonPublic | BindingFlags.Instance).GetValue(this)).Text;
+
+                if (unit != "")
+                {
+                    result += output + " " + unit + "\r\n";
+                }
+            }
+
+            return result;
+        }
+    }
+
+    public class StaticVariables
+    {
+        // Length Variables
+        double mm = 1000000, cm = 100000, m = 1000, km = 1, 
+            mi = 0.62137, ft = 3280.84, inch = 39370.1, yd = 1093.61;
+
+        // Digital Size Variables
+        double b = 8, B = 1, KB = 1024, MB = 1048576, GB = 1073741824.0005517,
+            TB = 1099511627775.9133, PB = 1125899906842782.8;
+
+        public string getValue(double i1, double i2, string m1, string m2, string m3)
+        {
+            if (m1 != "" && m2 != "" && m3 != "")
+            {
+                return (i1 * getRatio(m1, m3) + i2 * getRatio(m2, m3)).ToString();
+            }
+            else if (m1 != "" && m2 == "" && m3 != "")
+            {
+                return (i1 * getRatio(m1, m3)).ToString();
+            }
+            else if (m1 == "" && m2 != "" && m3 != "")
+            {
+                return (i2 * getRatio(m2, m3)).ToString();
+            }
+            else
+            {
+                return "";
+            }
+            
+        }
+
+        public double getRatio(string m1, string m2)
+        {
+            double v1 = (double) this.GetType().GetField(m1,
+                BindingFlags.NonPublic | BindingFlags.Instance).GetValue(this);
+            double v2 = (double) this.GetType().GetField(m2,
+                BindingFlags.NonPublic | BindingFlags.Instance).GetValue(this);
+            return v2 / v1;
+        }
+    }
+}
