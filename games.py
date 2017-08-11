d255a97b3d47905d730e14492e92d32dfbd54b44
Kris Lai <kriskirla@gmail.com>
2017-08-11 13:59:49 -0400

    Minor UI Changes, new functionality
    - Regression test completed
    - Adding one last functionality (Games) to Chakbot

---
+++ games.py
@@ -0,0 +1,16 @@
+def game_launcher(selection):
+    """ This function is to decide which game to launch
+
+    Args:
+        selection (str) : Name of the game
+
+    Returns:
+        Nothing
+    """
+    if selection == "tic tac toe":
+        tic_tac_toe()
+
+
+def tic_tac_toe():
+    pass
+
