94201c2f21e1338f531fb62f3de80b6de7fca964
Kris Lai <kriskirla@gmail.com>
2017-08-11 15:45:29 -0400

    Game added
    - First game arrived to Chakbot! (Tic Tac Toe)
    - This is still in beta, there will be difficulty levels (Easy will be randomly picking, hard is optimizing)
    
    Not stable release, do not use this as base.

--- Chakbot_Python/Chakbot_Main.py
+++ Chakbot_Python/Chakbot_Main.py
@@ -5,23 +5,8 @@ def response_decider():
     Returns:
         Nothing
     """
-    help_menu = str("ChakBot: " +
-                    "List of functions\r\n" +
-                    "====================================================\r\n" +
-                    "//date\t| Display the current time\r\n" +
-                    "//exit\t| Terminate Chakbot\r\n" +
-                    "teach\t| Teach chakbot. Optional teach <category> \"<request>\" \"<response>\"\r\n" +
-                    "encrypt\t| Encrypt a message. Optional: encrypt <message>\r\n" +
-                    "decrypt\t| Decrypt a message with the key. Optional: decrypt <message> <key>\r\n" +
-                    "morse\t| Encrypt/decrypt message into morse code. Optional: morse <message>\r\n" +
-                    "calculate\t| Calculator. Optional: calculate <formula>\r\n" +
-                    "convert\t| Convert units (#unit to unit). Optional: convert <number> <metric> to <metric>\r\n" +
-                    "====================================================\r\n")
-
-    print("====================================================\r\n" +
-          "Chakbot: Hello! My name is Chakbot! <(^ O ^ v) \r\n" +
-          "Type 'help' if you want to see the help menu.\r\n" +
-          "====================================================\r\n")
+    # Begin Application
+    print(INTRO)
 
     # User Input Analyzer
     while True:
@@ -29,14 +14,16 @@ def response_decider():
         user_input = input("User: ")
 
         if user_input.lower() == "help":
-            print(help_menu)
+            print(HELP_MENU)
+        elif user_input[:4].lower() == "help" and user_input[5:] in HELP_EX.keys():
+            print("{\r\n" + HELP_EX[user_input[5:]] + "\r\n}\r\n")
         elif user_input == "//date":
             # Display date and time
             print('Chakbot: ' + str(datetime.datetime.now()))
         elif user_input == "//exit":
             # Always check if user wants to terminate
             sys.exit()
-        elif user_input[0:9].lower() == "calculate":
+        elif user_input[:9].lower() == "calculate":
             # Determine if formula follows after calculate, if not then prompt
             if len(user_input) <= 9:
                 # Chakbot will request formula
@@ -44,21 +31,21 @@ def response_decider():
             else:
                 # Formula following calculate
                 print('Chakbot: ' + str(calculate(user_input[9:])))
-        elif user_input[0:7].lower() == "convert":
+        elif user_input[:7].lower() == "convert":
             if len(user_input) <= 7:
                 # Chakbot will request conversion
                 print('Chakbot: ' + str(convert(input("Chakbot: Sure, what do you want to convert?\r\nConversion: "))))
             else:
                 # Conversion following convert
                 print('Chakbot: ' + str(convert(user_input[7:])))
-        elif user_input[0:7].lower() == "encrypt":
+        elif user_input[:7].lower() == "encrypt":
             if len(user_input) <= 7:
                 # Chakbot will request message to encrypt
                 print('Chakbot: ' + str(encrypt(input("Chakbot: Sure, what's the message?\r\nMessage: "))))
             else:
                 # Conversion following encrypt
                 print('Chakbot: ' + str(encrypt(user_input[8:])))
-        elif user_input[0:7].lower() == "decrypt":
+        elif user_input[:7].lower() == "decrypt":
             if len(user_input) <= 7:
                 # Chakbot will request message and key to decrypt
                 message = input("Chakbot: Sure, what's the message?\r\nMessage: ")
@@ -67,14 +54,14 @@ def response_decider():
                 # Conversion following decrypt
                 print('Chakbot: ' + str(decrypt(user_input[8:user_input.rfind(' ')],
                                                 user_input[user_input.rfind(' ') + 1:])))
-        elif user_input[0:5].lower() == "morse":
+        elif user_input[:5].lower() == "morse":
             if len(user_input) <= 5:
                 # Chakbot will request message to translate to morse
                 print('Chakbot: ' + str(morse(input("Chakbot: Sure, what's the message?\r\nMessage: "))))
             else:
                 # Conversion following decrypt
                 print('Chakbot: ' + str(morse(user_input[6:])))
-        elif user_input[0:5].lower() == "teach":
+        elif user_input[:5].lower() == "teach":
             if len(user_input) <= 5:
                 # Chakbot will request message to translate to morse
                 categ = input("Chakbot: Sure, what's the category?\r\nCategory: ")
@@ -88,8 +75,16 @@ def response_decider():
                 request = user_input[sec_1: sec_1 + user_input[sec_1:].find("\"")]
                 sec_2 = sec_1 + len(request) + 3
                 response = user_input[sec_2:len(user_input) - 1]
-
                 print('Chakbot: ' + str(teach(categ, request, response)))
+        elif user_input[:4].lower() == "game":
+            if len(user_input) <= 4:
+                # Chakbot will request message to translate to morse
+                print('Chakbot: Sure, here are the games\r\n' +
+                      "1. tic tac toe")
+                print(str(game_launcher(input("Chakbot: Which game would you like to play?\r\nInput: "))))
+            else:
+                # Conversion following decrypt
+                print('Chakbot: ' + str(game_launcher(user_input[5:])))
         else:
             # Get reply from brain
             print("Chakbot: " + get_message(user_input))
@@ -98,9 +93,11 @@ def response_decider():
 if __name__ == "__main__":
     import sys
     import datetime
+    from text_output import *
     from learning import *
     from cryptography import *
     from extra_tools import *
+    from games import *
 
     # First Initialization
     read_brain()
