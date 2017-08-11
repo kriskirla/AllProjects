c91f7ef072156c12831e6468c670193ec3a3f063
Kris Lai <kriskirla@gmail.com>
2017-08-11 09:36:51 -0400

    Fixed minor issues
    - Regression testing in progress

--- Chakbot_Python/Chakbot_Main.py
+++ Chakbot_Python/Chakbot_Main.py
@@ -1,22 +1,6 @@
-import os
-import sys
-import datetime
-from Chakbot_Python.cryptography import *
-from Chakbot_Python.extra_tools import *
-from Chakbot_Python.learning import *
-
-
-def main():
-    path = os.getcwd() + "/brain.txt"
-    response_decider(path)
-
-
-def response_decider(path):
+def response_decider():
     """
     This will execute commands or reply base on user input
-
-    Args:
-        categories (dict) : All the content in brain.txt
 
     Returns:
         Nothing
@@ -39,9 +23,6 @@ def response_decider(path):
           "Type 'help' if you want to see the help menu.\r\n" +
           "====================================================\r\n")
 
-    # First Initialization
-    read_brain(path)
-
     # User Input Analyzer
     while True:
         # Get user input
@@ -59,29 +40,29 @@ def response_decider(path):
             # Determine if formula follows after calculate, if not then prompt
             if len(user_input) <= 9:
                 # Chakbot will request formula
-                print('Chakbot: ' + str(calculate(input("Chakbot: Sure, what is your formula?\r\nUser: "))))
+                print('Chakbot: ' + str(calculate(input("Chakbot: Sure, what is your formula?\r\nFormula: "))))
             else:
                 # Formula following calculate
                 print('Chakbot: ' + str(calculate(user_input[9:])))
         elif user_input[0:7].lower() == "convert":
             if len(user_input) <= 7:
                 # Chakbot will request conversion
-                print('Chakbot: ' + str(convert(input("Chakbot: Sure, what do you want to convert?\r\nUser: "))))
+                print('Chakbot: ' + str(convert(input("Chakbot: Sure, what do you want to convert?\r\nConversion: "))))
             else:
                 # Conversion following convert
                 print('Chakbot: ' + str(convert(user_input[7:])))
         elif user_input[0:7].lower() == "encrypt":
             if len(user_input) <= 7:
                 # Chakbot will request message to encrypt
-                print('Chakbot: ' + str(encrypt(input("Chakbot: Sure, what's the message?\r\nUser: "))))
+                print('Chakbot: ' + str(encrypt(input("Chakbot: Sure, what's the message?\r\nMessage: "))))
             else:
                 # Conversion following encrypt
                 print('Chakbot: ' + str(encrypt(user_input[8:])))
         elif user_input[0:7].lower() == "decrypt":
             if len(user_input) <= 7:
                 # Chakbot will request message and key to decrypt
-                message = input("Chakbot: Sure, what's the message?\r\nUser: ")
-                print('Chakbot: ' + str(decrypt(message, input("Chakbot: What's the key?\r\nUser: "))))
+                message = input("Chakbot: Sure, what's the message?\r\nMessage: ")
+                print('Chakbot: ' + str(decrypt(message, input("Chakbot: What's the key?\r\nKey: "))))
             else:
                 # Conversion following decrypt
                 print('Chakbot: ' + str(decrypt(user_input[8:user_input.rfind(' ')],
@@ -89,17 +70,17 @@ def response_decider(path):
         elif user_input[0:5].lower() == "morse":
             if len(user_input) <= 5:
                 # Chakbot will request message to translate to morse
-                print('Chakbot: ' + str(morse(input("Chakbot: Sure, what's the message?\r\nUser: "))))
+                print('Chakbot: ' + str(morse(input("Chakbot: Sure, what's the message?\r\nMessage: "))))
             else:
                 # Conversion following decrypt
                 print('Chakbot: ' + str(morse(user_input[6:])))
         elif user_input[0:5].lower() == "teach":
             if len(user_input) <= 5:
                 # Chakbot will request message to translate to morse
-                categ = input("Chakbot: Sure, what's the category?\r\nUser: ")
-                request = input("Chakbot: Okay, what's the request?\r\nUser: ")
-                response = input("Chakbot: How should I response to it?\r\nUser: ")
-                print('Chakbot: ' + str(teach(path, categ, request, response)))
+                categ = input("Chakbot: Sure, what's the category?\r\nCategory: ")
+                request = input("Chakbot: Okay, what's the request?\r\nMessage: ")
+                response = input("Chakbot: How should I response to it?\r\nMessage: ")
+                print('Chakbot: ' + str(teach(categ, request, response)))
             else:
                 # Conversion following decrypt
                 categ = user_input[6:][:user_input[6:].find(' ')]
@@ -108,11 +89,19 @@ def response_decider(path):
                 sec_2 = sec_1 + len(request) + 3
                 response = user_input[sec_2:len(user_input) - 1]
 
-                print('Chakbot: ' + str(teach(path, categ, request, response)))
+                print('Chakbot: ' + str(teach(categ, request, response)))
         else:
             # Get reply from brain
             print("Chakbot: " + get_message(user_input))
 
 
-# Runs the code at launch
-main()
+if __name__ == "__main__":
+    import sys
+    import datetime
+    from learning import *
+    from cryptography import *
+    from extra_tools import *
+
+    # First Initialization
+    read_brain()
+    response_decider()
